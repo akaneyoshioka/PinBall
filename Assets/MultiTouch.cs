b2be55d9c7271ca5d43ad2b77d8d9c9bb3637982
吉岡茜 <sode_take@icloud.com>
2018-09-03 22:07:57 +0900

    PinBallプロジェクト初めてのコミット
    iPhoneでフリッパーが動かないので、確認にために作成。

---
+++ Assets/MultiTouch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiTouch : MonoBehaviour {
+	//HingiJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+	//初期の傾き
+	private float defaultAngle=20;
+	//弾いた時の傾き
+	private float flickAngle=-20;
+
+	int rightFingerId;
+	int leftFingerId;
+
+	//Use this for initialization
+	void start(){
+		this.myHingeJoint = GetComponent<HingeJoint>();
+		SetAngle (this.defaultAngle);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < Input.touchCount; i++) {
+			int id = Input.touches [i].fingerId;
+			Vector2 pos = Input.touches [i].position;
+		
+			if (Input.touches [i].phase == TouchPhase.Began) {
+				if (pos.x > Screen.width / 2 && tag == "RightFripperTag") {
+					rightFingerId=id;
+					SetAngle (this.flickAngle);
+				} else if (pos.x < Screen.width / 2 && tag == "LeftFripperTag") {
+					leftFingerId=id;
+					SetAngle (this.flickAngle);
+				}
+			} else if (Input.touches [i].phase == TouchPhase.Ended) {
+				if (id == rightFingerId && tag == "RightFripperTag") {
+					SetAngle (this.defaultAngle);
+				} else if (id == leftFingerId && tag == "LeftFripperTag") {
+					SetAngle (this.defaultAngle);
+				}
+			}
+		}
+	}
+		
+	public void SetAngle(float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition=angle;
+		this.myHingeJoint.spring = jointSpr;
+	}
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+	private GameObject scoreText;
+
+	public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		this.scoreText = GameObject.Find ("ScoreText");
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "LargeStarTag") {
+			AddScore (20);
+		} else if (other.gameObject.tag == "LargeCloudTag") {
+			AddScore (20);
+		} else if (other.gameObject.tag == "SmallStarTag") {
+			AddScore (10);
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			AddScore (10);
+		} else {
+			AddScore (0);
+		}
+	}
+
+	void AddScore(int score){
+		this.score += score;
+		this.scoreText.GetComponent<Text> ().text = "Score:" + this.score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
